d11537fa34473251ed268e8b5c5318e0be2bbd58
Nicolas D <oc_nicolasd>
2023-04-03 16:41:41 +0200

    feature: Just for the presentation add box with links of each profil and wrong link to show wrong page #18
    Resolved #18

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,3 +1,15 @@
+import { NavLink } from 'react-router-dom'
+import classes from './HomePage.module.css'
+
 export default function HomePage() {
-  return <>Accueil</>
+  return (
+    <section className={ classes.container }>
+      <ul>
+        <li><h2>Choisir un profil</h2></li>
+        <li><NavLink to="/profile/12">Profil 12</NavLink></li>
+        <li><NavLink to="/profile/18">Profil 18</NavLink></li>
+        <li><NavLink to="/profile/42">Profil inexistant</NavLink></li>
+      </ul>
+    </section>
+  )
 }
